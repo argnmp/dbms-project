7d547bd030b064238c34e18062d21b6f39795cf7
KimTaehyeon <kimtahen@hanyang.ac.kr>
2022-10-16 23:33:41 +0900

    gitignore modify

--- project3/db_project/build/db/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kimtahen/workspace/2022_ite2038_2019097210/project3/db_project/db
-# Build directory: /home/kimtahen/workspace/2022_ite2038_2019097210/project3/db_project/build/db
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
