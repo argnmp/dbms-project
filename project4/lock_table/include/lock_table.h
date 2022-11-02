a11ee3b60538e19523d68ca79d302a40a55b188c
KimTaehyeon <kimtahen@hanyang.ac.kr>
2022-11-02 14:43:30 +0900

    lock table init

---
+++ project4/lock_table/include/lock_table.h
@@ -0,0 +1,43 @@
+#ifndef __LOCK_TABLE_H__
+#define __LOCK_TABLE_H__
+
+#include <stdint.h>
+#include <unordered_map>
+#include <map>
+#include <iostream>
+using namespace std;
+
+typedef struct lock_t lock_t;
+
+// https://gist.github.com/eugene-malashkin/884e225ff57aca1b9cbe
+// This is the hash combine function originated from boost::hash_combine
+template <class T>
+inline void boost_hash_combine(std::size_t& seed, const T& v){
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+struct pair_for_hash {
+    template <class T1, class T2>
+        std::size_t operator () (const std::pair<T1, T2> &p) const {
+            std::size_t seed = 0;
+            boost_hash_combine(seed, p.first);
+            boost_hash_combine(seed, p.second);
+            return seed;
+        }
+};
+struct hash_table_entry {
+    int64_t table_id;
+    int64_t key;
+    lock_t* head;
+    lock_t* tail;
+};
+extern unordered_map<pair<int64_t,int64_t>, hash_table_entry, pair_for_hash> hash_table;
+
+
+/* APIs for lock table */
+int init_lock_table();
+lock_t *lock_acquire(int64_t table_id, int64_t key);
+int lock_release(lock_t* lock_obj);
+
+#endif /* __LOCK_TABLE_H__ */
---
+++ project4/lock_table/src/lock_table.cc
@@ -0,0 +1,95 @@
+#include "lock_table.h"
+
+
+struct lock_t {
+    lock_t* prev;
+    lock_t* next;
+    hash_table_entry* sentinel;
+    pthread_cond_t cond;
+};
+
+typedef struct lock_t lock_t;
+
+unordered_map<pair<int64_t,int64_t>, hash_table_entry, pair_for_hash> hash_table;
+
+pthread_mutex_t lock_table_latch;
+
+int init_lock_table() {
+    pthread_mutexattr_t mtx_attribute;
+    pthread_mutexattr_init(&mtx_attribute);
+    pthread_mutexattr_settype(&mtx_attribute, PTHREAD_MUTEX_NORMAL);
+    pthread_mutex_init(&lock_table_latch, &mtx_attribute);
+    return 0;
+}
+
+lock_t* lock_acquire(int64_t table_id, int64_t key) {
+    bool is_immediate;
+    pthread_mutex_lock(&lock_table_latch); 
+    if(hash_table.find({table_id, key})==hash_table.end()){
+        hash_table_entry hte;
+        hte.table_id = table_id;
+        hte.key = key;
+        hte.head = nullptr;
+        hte.tail = nullptr;
+        hash_table.insert({{table_id, key}, hte}); 
+    }
+    hash_table_entry* target = &hash_table[{table_id, key}]; 
+
+    is_immediate = (target->head == nullptr);
+    lock_t* lck = new lock_t;
+    lck->next = nullptr;
+    lck->prev = nullptr;
+    lck->sentinel = &hash_table[{table_id, key}];
+    lck->cond = PTHREAD_COND_INITIALIZER;
+    if(is_immediate){
+        target->head = lck;
+        target->tail = lck;
+    }
+    else {
+        target->tail->next = lck;
+        lck->prev = target->tail;
+        target->tail = lck; 
+    }
+    if(!is_immediate){
+        pthread_cond_wait(&lck->cond, &lock_table_latch);
+    }
+    pthread_mutex_unlock(&lock_table_latch); 
+
+    /*
+    //this is dummy mutex, because each lock hash one conditional variable
+    pthread_mutex_t cond_mutex;
+    pthread_mutexattr_t mtx_attribute;
+    pthread_mutexattr_init(&mtx_attribute);
+    pthread_mutexattr_settype(&mtx_attribute, PTHREAD_MUTEX_NORMAL);
+    pthread_mutex_init(&cond_mutex, &mtx_attribute);
+    */
+    
+    return lck;
+};
+
+int lock_release(lock_t* lock_obj) {
+    hash_table_entry* target = lock_obj->sentinel;
+    bool is_immediate;
+
+    pthread_mutex_lock(&lock_table_latch); 
+
+    is_immediate = (lock_obj->next == nullptr);
+
+    if(is_immediate){
+        target->head = nullptr;
+        target->tail = nullptr;
+    }
+    else {
+        target->head = lock_obj->next;
+        lock_obj->next->prev = nullptr;
+    }
+    
+    pthread_mutex_unlock(&lock_table_latch); 
+
+    if(!is_immediate){
+        pthread_cond_signal(&lock_obj->next->cond);
+    }
+    delete lock_obj;
+    
+    return 0;
+}
